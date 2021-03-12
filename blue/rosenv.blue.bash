d88f5c9293f25163749926a5ccb5baa7119c7ac9
Ian Peake <ian.peake@rmit.edu.au>
2021-03-12 13:42:06 +1100

    experimental update (will require manual intervention)

--- blue/rosenv.blue.bash
+++ blue/rosenv.blue.bash
@@ -1,10 +1,12 @@
 #!/bin/bash
 
+source /ws/devel/setup.bash
+
 HOSTNAME=`hostname`
 export ROS_IP=`host ${HOSTNAME} | sed 's/.* //g'`
 export ROS_MASTER_URI=http://$ROS_IP:11311
 
-export ROSIE_MASTER_IP=`host vxlab-rosie | sed 's/.* //g'`
+export ROSIE_MASTER_IP=`host gazebo | sed 's/.* //g'`
 export ROSIE_MASTER_URI=http://$ROSIE_MASTER_IP:11311
 
 export MB_LASER_BIRDCAGE_R2000=1
